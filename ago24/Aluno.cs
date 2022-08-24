a96759c8b4e394892455d982deae93d1f76aafa6
Isabela Bianca <79585893+isabelabianca@users.noreply.github.com>
2022-08-24 09:53:41 -0300

    Add files via upload

---
+++ ago24/Aluno.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ago24
+{
+    public struct Aluno{
+        public string nome { get; set; }
+        public decimal nota { get; set; }
+    }
+}
---
+++ ago24/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ago24
+{
+    public class Program {
+        public static void Main(string[] args)
+        {
+            string opcaoUsuario = obterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("Opção inválida.");
+                }
+                opcaoUsuario = obterOpcaoUsuario();
+            }
+        }
+
+        private static string obterOpcaoUsuario()
+        {
+            Console.WriteLine("\nInforme a opção desejada:");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+
+            string opcaoUsuario = Console.ReadLine();
+
+            return opcaoUsuario;
+        }
+    }
+}
